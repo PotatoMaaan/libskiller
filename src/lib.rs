ad34ec3bc43cbb82d100882e4546e464df15abbc
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2023-09-22 15:03:52 +0200

    polling rate and docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,33 @@
-use std::time::Duration;
+//! This library provides methods to change settings on the Skiller Pro+ keyboard
+//!
+//! To get started, initialize the SkillerProPlus struct using the `new` method.
+//!
+//! To use the types in this libary with clap, activate the `clap` feature.
+//!
+//! ## Note
+//! Since i only have one of these keyboard available to test,
+//! i won't support selecting multiple devices for now, as i
+//! have no way to test this feature.
 
 use rusb::{request_type, Context, DeviceHandle, Direction, Recipient, RequestType, UsbContext};
+use std::time::Duration;
 
 const INTERFACE: u8 = 1;
 const VID: u16 = 0x04d9;
 const PID: u16 = 0xa096;
 
+trait ToSkillerBytes {
+    fn to_skiller_bytes(&self) -> u8;
+}
+
+/// A struct that can interact with the skiller pro plus keyboard
 #[derive(Debug, PartialEq, Eq)]
 pub struct SkillerProPlus {
     handle: DeviceHandle<rusb::Context>,
     timeout: Duration,
 }
 
+/// Represents the different LED colors of the keyboard
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Color {
@@ -24,8 +40,8 @@ pub enum Color {
     White,
 }
 
-impl Color {
-    const fn to_skiller_offset(&self) -> u8 {
+impl ToSkillerBytes for Color {
+    fn to_skiller_bytes(&self) -> u8 {
         match self {
             Color::Red => 0,
             Color::Green => 1,
@@ -38,6 +54,7 @@ impl Color {
     }
 }
 
+/// Represents one of the three profiles of the keyboard
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Profile {
@@ -46,8 +63,8 @@ pub enum Profile {
     P3,
 }
 
-impl Profile {
-    const fn to_skiller_offset(&self) -> u8 {
+impl ToSkillerBytes for Profile {
+    fn to_skiller_bytes(&self) -> u8 {
         match self {
             Profile::P1 => 1,
             Profile::P2 => 2,
@@ -56,6 +73,7 @@ impl Profile {
     }
 }
 
+/// Represents one of the possible brightness settings of the keyboard
 #[cfg_attr(feature = "clap", derive(clap::Subcommand))]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Brightness {
@@ -67,6 +85,36 @@ pub enum Brightness {
 
     /// All colors pulsatinng in a cycle
     Cycle,
+}
+
+/// Represents one of four valid polling rates of the keyboard
+#[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub enum PollingRate {
+    HZ125,
+    HZ250,
+    HZ500,
+    HZ1000,
+}
+
+impl ToSkillerBytes for PollingRate {
+    fn to_skiller_bytes(&self) -> u8 {
+        match self {
+            PollingRate::HZ125 => 8,
+            PollingRate::HZ250 => 4,
+            PollingRate::HZ500 => 2,
+            PollingRate::HZ1000 => 1,
+        }
+    }
+}
+
+impl ToSkillerBytes for bool {
+    fn to_skiller_bytes(&self) -> u8 {
+        match self {
+            true => 0,
+            false => 1,
+        }
+    }
 }
 
 impl SkillerProPlus {
@@ -103,7 +151,7 @@ impl SkillerProPlus {
     /// Sets the color of the keyboard for the given profile
     /// Returns the amount of bytes written or any libusb errors
     pub fn set_color(&self, color: Color, profile: Profile) -> rusb::Result<usize> {
-        let p = profile.to_skiller_offset();
+        let p = profile.to_skiller_bytes();
 
         let payload: [u8; 8] = [
             0x07,
@@ -112,13 +160,13 @@ impl SkillerProPlus {
             0x0a,
             0x04,
             0x00,
-            color.to_skiller_offset(),
+            color.to_skiller_bytes(),
             0x00,
         ];
 
         let mut total_written = 0;
 
-        total_written += self.skiller_write(&switch_profile_profile(p))?;
+        total_written += self.skiller_write(&switch_profile(p))?;
         total_written += self.skiller_write(&payload)?;
 
         return Ok(total_written);
@@ -128,7 +176,7 @@ impl SkillerProPlus {
     ///
     /// Returns the amount of bytes written or any error returned by libusb
     pub fn set_profile(&self, profile: Profile) -> rusb::Result<usize> {
-        Ok(self.skiller_write(&switch_profile_profile(profile.to_skiller_offset()))?)
+        Ok(self.skiller_write(&switch_profile(profile.to_skiller_bytes()))?)
     }
 
     /// Sets the brightness and color for the given profile.
@@ -137,7 +185,7 @@ impl SkillerProPlus {
     /// ## Note
     /// You have to provide the color as well because the keyboards API is weird
     pub fn set_brightness(&self, brightness: Brightness, profile: Profile) -> rusb::Result<usize> {
-        let p = profile.to_skiller_offset();
+        let p = profile.to_skiller_bytes();
 
         let payload: [u8; 8] = match brightness {
             Brightness::Static { level, color } => [
@@ -147,7 +195,7 @@ impl SkillerProPlus {
                 level,
                 0x04,
                 0x00,
-                color.to_skiller_offset(),
+                color.to_skiller_bytes(),
                 0x00,
             ],
             Brightness::Pulsating { color } => [
@@ -157,7 +205,7 @@ impl SkillerProPlus {
                 11,
                 0x04,
                 0x00,
-                color.to_skiller_offset(),
+                color.to_skiller_bytes(),
                 0x00,
             ],
             Brightness::Cycle => [0x07, 0x0a, p, 12, 0x04, 0x00, 0x00, 0x00],
@@ -165,27 +213,43 @@ impl SkillerProPlus {
 
         let mut total_written = 0;
 
-        total_written += self.skiller_write(&switch_profile_profile(p))?;
+        total_written += self.skiller_write(&switch_profile(p))?;
         total_written += self.skiller_write(&payload)?;
 
         return Ok(total_written);
+    }
+
+    /// Sets the global polling rate of the keyboard.
+    ///
+    /// Returns the amount of bytes written or any error returned by libusb.
+    pub fn set_polling_rate(&self, rate: PollingRate) -> rusb::Result<usize> {
+        Ok(self.skiller_write(&[
+            0x07,
+            0x01,
+            rate.to_skiller_bytes(),
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+        ])?)
     }
 
     /// Sets the windows key to be enabled or disabled
     ///
     /// Returns the amount of bytes written or any error returned by libusb
     pub fn set_win_key(&self, enable: bool, profile: Profile) -> rusb::Result<usize> {
-        let p = profile.to_skiller_offset();
-        let enable = match enable {
-            true => 0,
-            false => 1,
-        };
+        let p = profile.to_skiller_bytes();
+        let e = enable.to_skiller_bytes();
 
-        let payload = [0x07, 0x0b, p, enable, 0x00, 0x00, 0x00, 0x00];
+        let payload = [0x07, 0x0b, p, e, 0x00, 0x00, 0x00, 0x00];
 
         Ok(self.skiller_write(&payload)?)
     }
 
+    /// Writes data to the keyboard
+    ///
+    /// Returns the amount of bytes written or any error returned by libusb
     fn skiller_write(&self, data: &[u8; 8]) -> rusb::Result<usize> {
         let rt = request_type(Direction::Out, RequestType::Class, Recipient::Interface);
 
@@ -196,19 +260,18 @@ impl SkillerProPlus {
     }
 }
 
-fn switch_profile_profile(profile: u8) -> [u8; 8] {
+fn switch_profile(profile: u8) -> [u8; 8] {
     [0x07, 0x02, profile, 0x00, 0x00, 0x00, 0x00, 0x00]
 }
 
-fn begin_handshake_profile(profile: u8) -> [u8; 8] {
-    [0x07, 0x0b, profile, 0x00, 0x00, 0x00, 0x00, 0x00]
-}
+// fn begin_handshake_profile(profile: u8) -> [u8; 8] {
+//     [0x07, 0x0b, profile, 0x00, 0x00, 0x00, 0x00, 0x00]
+// }
 
-// No idea what this does?
-fn footer() -> [u8; 8] {
-    let mut cmd = [0; 8];
-    cmd[0] = 0x07;
-    cmd[1] = 0x02;
-    cmd[2] = 0x04;
-    return cmd;
-}
+// fn footer() -> [u8; 8] {
+//     let mut cmd = [0; 8];
+//     cmd[0] = 0x07;
+//     cmd[1] = 0x02;
+//     cmd[2] = 0x04;
+//     return cmd;
+// }
